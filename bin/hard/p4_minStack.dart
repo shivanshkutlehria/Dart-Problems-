d38ed5987c5890abeb5a606e6d88f9eb99c2884a
shivanshkutlehria <kutlehriashivansh2006@gmail.com>
2025-06-14 10:32:13 +0530

    Implemented MinStack Problem in O(1) time

---
+++ bin/hard/p4_minStack.dart
@@ -0,0 +1,42 @@
+// Implement a Min Stack
+
+class Stack {
+  List<int> _stack = [];
+  List<int> _minstack = [];
+
+  void push(int element) {
+    _stack.add(element);
+    if (_minstack.isEmpty || element <= _minstack.last) {
+      _minstack.add(element);
+    } else {
+      _minstack.add(_minstack.last);
+    }
+  }
+
+  void pop() {
+    if (_stack.isNotEmpty) {
+      _minstack.remove(_minstack.last);
+      _stack.remove(_stack.last);
+    }
+  }
+
+  int? top() {
+    return _stack.isNotEmpty ? _stack.last : null;
+  }
+
+  int? getmin(){
+    return _minstack.isNotEmpty ? _minstack.last : null;
+  }
+}
+void main() {
+  Stack stack1 = Stack();
+  stack1.push(5);
+  stack1.push(3);
+  stack1.push(7);
+  stack1.push(2);
+
+  print(stack1.getmin()); 
+  stack1.pop();
+  print(stack1.getmin()); 
+  print(stack1.top());    
+}
